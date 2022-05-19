17ae31498869a94d290c6dd9f7119d899b47c726
RahmatSetia <rahmasetiq@gmail.com>
2022-05-19 11:36:30 +0700

    try new endpoint

--- src/main/java/com/alterra/mini_project/MiniProjectApplication.java
+++ src/main/java/com/alterra/mini_project/MiniProjectApplication.java
@@ -2,12 +2,20 @@ package com.alterra.mini_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+
+	@GetMapping("/coba")
+	public String pub(){
+		return "try new endpoint";
 	}
 
 }
